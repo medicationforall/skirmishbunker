31646cc430e594ef1584131a33e2aa6d4a6c0323
medicationforall <inklink28@gmail.com>
2022-12-23 10:05:44 -0500

    Updated bunker floor to use optimized method.

--- src/skirmishbunker/bunkerFloor.py
+++ src/skirmishbunker/bunkerFloor.py
@@ -4,7 +4,7 @@ from math import floor as math_floor
 
 def init_floor_params(self):
     self.render_floor_tiles=True
-    self.floor_padding = 20
+    self.floor_padding = 0
     self.floor_tile_size = 11
     self.floor_tile_height = 1
     self.floor_chamfer_size = 2.4
@@ -19,7 +19,7 @@ def make_interior_floor(self):
     int_length = self.int_length-self.floor_padding
     int_width = self.int_width-self.floor_padding
 
-    floor_tile = tile.octagon_with_dots(tile_size, self.floor_chamfer_size, self.floor_mid_tile_size, tile_padding, self.floor_tile_height)
+    floor_tile = tile.octagon_with_dots_2(tile_size, self.floor_chamfer_size, self.floor_mid_tile_size, tile_padding, self.floor_tile_height)
 
     columns = math_floor(int_width/(tile_size + tile_padding))
     rows = math_floor(int_length/(tile_size + tile_padding))
