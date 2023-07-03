ca8b3cbf195df1da4ea80c08d4bb1bc956afb521
medicationforall <inklink28@gmail.com>
2023-07-03 19:42:23 -0400

    Added bunker example 4 for feature request testing.

---
+++ example/bunker4_large_inset.py
@@ -0,0 +1,39 @@
+import cadquery as cq
+from skirmishbunker import Bunker
+
+bp = Bunker()
+bp.inset=50
+bp.width=150
+bp.length=150
+bp.height=65
+
+bp.render_panel_details=True
+bp.panel_length=28
+bp.panel_width = 6
+bp.panel_padding = 4
+
+bp.render_windows=True
+bp.skip_windows = []
+bp.window_length = 15
+bp.window_height = 8
+#bp.window_width =
+bp.window_frame_chamfer = 1.6
+bp.window_frame_chamfer_select = "<Z"
+
+bp.render_doors=False
+bp.door_panels = [0, 5]
+
+bp.render_ladders=False
+bp.ladder_panels = [8]
+
+bp.render_floor_tiles=False
+bp.render_roof=False
+
+
+bp.make()
+rec = bp.build_plate()
+
+#show_object(rec)
+
+
+cq.exporters.export(rec,'stl/bunker4_large_inset.stl')
