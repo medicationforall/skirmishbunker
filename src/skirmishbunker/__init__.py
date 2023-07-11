3cf2d3a785c5dae320d59070c6c1c2f3a2ddb1ea
Morven Lewis-Everley <morven@i-lateral.com>
2023-07-11 11:36:25 +0100

    Rename roof to detailed roof

--- src/skirmishbunker/__init__.py
+++ src/skirmishbunker/__init__.py
@@ -2,7 +2,7 @@ from .Base import Base
 from .Bunker import Bunker
 from .BlastDoor import BlastDoor
 from .SplitDoor import SplitDoor
-from .Roof import Roof
+from .DetailedRoof import DetailedRoof
 from .Hatch import Hatch
 from .Catwalk import Catwalk
 from .FlatRoof import FlatRoof
